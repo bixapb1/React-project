3eb299dcb33a79c479c76db27f434a57c22d6136
Bixapb <Bixapb@gmail.com>
2021-09-08 22:14:13 +0300

    homework 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Header from "./components/Header";
+import Layout from "./components/Layout";
+import Footer from "./components/Footer";
+import SunAndSea from "./assets/Layout/bg3.jpg";
+import Pokemon from "./assets/Layout/bg2.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World, React.JS!</p>
-      </header>
-    </div>
+    <>
+      <Header title="Hello, world!" descr="Intro to React" />
+      <Layout
+        title="This is block one"
+        descr="Hello, world! Hello, world! Hello, world!"
+        urlBg={SunAndSea}
+      />
+      <Layout
+        title="This is block two"
+        descr="Hello, world! Hello, world! Hello, world!"
+        colorBg="grey"
+      />
+      <Layout
+        title="This is block three"
+        descr="Hello, world! Hello, world! Hello, world!"
+        urlBg={Pokemon}
+      />
+      <Footer />
+    </>
   );
 }
 
